9f7ef96924f3f2f0c782d86ef3cd5f0ce68391d3
Aswin Sahlan Himawan <ashymee@gmail.com>
2023-07-08 20:11:08 +0700

    chore: change hard-coded routePaths to router.routes

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,29 +1,32 @@
-import { Link } from "react-router-dom";
-
-const routePaths = [
-  {
-    path: "/",
-    title: "Home",
-  },
-  {
-    path: "/about",
-    title: "About",
-  },
-];
+import router from "@/router";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Header() {
+  const location = useLocation();
+
   return (
     <nav className="flex-none w-full h-10 flex items-center justify-center border-b-2">
       <ul className="flex items-center justify-center gap-x-2">
-        {routePaths.map((item, index) => (
-          <li key={index}>
-            {item ? (
-              <Link to={item.path} title={item.title}>
-                {item.title}
-              </Link>
-            ) : null}
-          </li>
-        ))}
+        {router.routes.map((item, index) => {
+          const path = item.path?.split("/")[1] as string;
+          const title = path === "" ? "Home" : path;
+          return (
+            <li key={index}>
+              {item ? (
+                <Link
+                  to={item.path as string}
+                  title={title}
+                  className={`capitalize ${location.pathname === item.path
+                      ? "text-blue-500"
+                      : "hover:text-blue-300"
+                    }`}
+                >
+                  {title}
+                </Link>
+              ) : null}
+            </li>
+          );
+        })}
       </ul>
     </nav>
   );
