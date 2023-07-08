f2ff41fa164c03d2e9fec47d35e07985caf9a95f
Aswin Sahlan Himawan <ashymee@gmail.com>
2023-07-08 20:10:03 +0700

    chore: add types declaration

--- src/components/layout/Wrapper.tsx
+++ src/components/layout/Wrapper.tsx
@@ -1,10 +1,6 @@
-import { ReactNode } from "react";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
-
-type WrapperProps = {
-  children: ReactNode;
-};
+import type { WrapperProps } from "@/index";
 
 export default function Wrapper({ children }: WrapperProps) {
   return (
