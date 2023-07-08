0b4bc253998da65967fd73c8e9486561fc168bda
Aswin Sahlan Himawan <ashymee@gmail.com>
2023-07-08 11:06:58 +0700

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
