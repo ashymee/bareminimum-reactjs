e0816201bc33b3c47b33e1fa46a771bec0067cae
Aswin Himawan <ashymee@gmail.com>
2025-04-19 02:48:43 +0700

    chore: update eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,35 +1,93 @@
-/* eslint-env node */
+// eslint.config.ts
+import js from "@eslint/js";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
-module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:react-hooks/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: "./tsconfig.json",
-    tsconfigRootDir: __dirname
+export default [
+  // Konfigurasi dasar dari ESLint
+  js.configs.recommended,
+
+  // Konfigurasi TypeScript
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname
+      },
+      globals: {
+        document: "readonly",
+        console: "readonly",
+        React: "readonly"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+      react: reactPlugin
+    },
+    settings: {
+      react: {
+        version: "detect" // Otomatis mendeteksi versi React
+      }
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      "@typescript-eslint/no-non-null-assertion": "off", // Contoh aturan kustom
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error"
+    }
   },
-  plugins: ["react-refresh"],
-  rules: {
-    "react-refresh/only-export-components": [
-      "warn",
-      { allowConstantExport: true }
-    ],
-    "@typescript-eslint/no-non-null-assertion": "off"
+
+  // Konfigurasi React Hooks
+  {
+    plugins: {
+      "react-hooks": reactHooksPlugin
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn"
+    }
   },
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project: "./tsconfig.json"
+
+  // Environment Node.js untuk file JavaScript
+  {
+    files: ["*.js", "*.cjs"],
+    languageOptions: {
+      globals: {
+        module: "readonly",
+        require: "readonly",
+        process: "readonly",
+        __dirname: "readonly"
       }
     }
   },
-  ignores: ["node_modules/", "dist/"]
-};
+
+  // Environment Node.js untuk file TypeScript
+  {
+    files: ["*.ts"],
+    languageOptions: {
+      globals: {
+        __dirname: "readonly"
+      }
+    }
+  },
+
+  // Konfigurasi khusus untuk file test
+  {
+    files: ["__test__/**/*.ts"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: null // Nonaktifkan type checking untuk file test
+      }
+    }
+  },
+
+  // Mengabaikan file tertentu
+  {
+    ignores: ["node_modules/", "dist/"]
+  }
+];
